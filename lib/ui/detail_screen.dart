ea5c13940a3797be1d31f3bffa498843e55c3379
Ignatius Odi <ignodi@gmail.com>
2023-05-22 20:34:27 +0700

    detail menu on progress

--- lib/ui/detail_screen.dart
+++ lib/ui/detail_screen.dart
@@ -1,241 +1,104 @@
+import 'package:badges/badges.dart' as badges;
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/database.dart';
+import 'package:go_router/go_router.dart';
+
 import '../models/menu.dart';
 
 class DetailScreen extends StatefulWidget {
-  static const routeName = '/detail';
-  final Menu menu;
-  final String id;
+  final int id;
 
-  const DetailScreen({Key? key, required this.menu, required this.id}) : super(key: key);
+  const DetailScreen({Key? key, required this.id}) : super(key: key);
 
   @override
   State<DetailScreen> createState() => _DetailScreenState();
 }
 
 class _DetailScreenState extends State<DetailScreen> {
-  int qty = 0;
-
   @override
   Widget build(BuildContext context) {
+    final menuQuery = FirebaseDatabase.instance
+        .ref("menu")
+        .orderByChild("menuId")
+        .equalTo(widget.id);
+
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Stack(
-                children: <Widget>[
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: 400,
-                    child: Image.network(widget.menu.imageUrl2,fit: BoxFit.cover),
-                  ),
-                  SafeArea(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: CircleAvatar(
-                        backgroundColor: Colors.white,
-                        child: IconButton(
-                          icon: const Icon(
-                            Icons.arrow_back,
-                            color: Colors.black,
-                          ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 350),
-                    padding: const EdgeInsets.all(16.0),
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height - 350,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
-                      color: Color(0xFFFAFAFA),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.menu.name,
-                            style: const TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 5
-                          ),
-                          Text(
-                            widget.menu.category,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Colors.black
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 15
-                          ),
-                          const Text(
-                            "Description:",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.grey
-                            ),
-                          ),
-                          Text(
-                            widget.menu.description,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Colors.grey
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 15
-                          ),
-                          const SizedBox(
-                            height: 10
-                          ),
-                          Row(
-                            children: [
-                              Text(
-                                "Rp ${widget.menu.price}",
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Expanded(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                  Row(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      SizedBox(
-                                        width: 30,
-                                        height: 30,
-                                        child: ElevatedButton(
-                                          onPressed: () {
-                                            setState(() {
-                                              if(qty > 0) {
-                                                qty--;
-                                              }
-                                            });
-                                          },
-                                          style: ElevatedButton.styleFrom(
-                                            backgroundColor: Colors.brown,
-                                            shape: const RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.only(topLeft: Radius.circular(5.0), bottomLeft: Radius.circular(5.0)),
-                                            ),
-                                          ),
-                                          child: const Text(
-                                              '-',
-                                              textAlign: TextAlign.center,
-                                              style: TextStyle(
-                                                  fontSize: 16,
-                                                  color: Colors.white
-                                              )
-                                          ),
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        width: 50,
-                                        height: 30,
-                                        child: Container(
-                                          decoration: const BoxDecoration(
-                                              border: Border(
-                                                left: BorderSide(width: 0.5, color: Colors.brown),
-                                                right: BorderSide(width: 0.5, color: Colors.brown),
-                                              ),
-                                              color: Colors.white
-                                          ),
-                                          child: Center(
-                                            child: Text(
-                                              qty.toString(),
-                                              style: const TextStyle(
-                                                fontSize: 16,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        width: 30,
-                                        height: 30,
-                                        child: ElevatedButton(
-                                          onPressed: () {
-                                            setState(() {
-                                              qty++;
-                                            });
-                                          },
-                                          style: ElevatedButton.styleFrom(
-                                            backgroundColor: Colors.brown,
-                                            shape: const RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.only(topRight: Radius.circular(5.0), bottomRight: Radius.circular(5.0)),
-                                            ),
-                                          ),
-                                          child: const Text(
-                                              '+',
-                                              textAlign: TextAlign.center,
-                                              style: TextStyle(
-                                                  fontSize: 16,
-                                                  color: Colors.white
-                                              )
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                    const SizedBox(
-                                        width: 10
-                                    ),
-                                    SizedBox(
-                                      width: 120,
-                                      height: 30,
-                                      child: ElevatedButton(
-                                          onPressed: (){
-                                            if(qty>0){
-                                              ScaffoldMessenger.of(context).showSnackBar(
-                                                  SnackBar(content: Text("${qty.toString()} ${widget.menu.name} added to cart"))
-                                              );
-                                              Navigator.pop(context);
-                                            }
-                                            else{
-                                              ScaffoldMessenger.of(context).showSnackBar(
-                                                const SnackBar(content: Text("Please add quantity"))
-                                              );
-                                            }
-                                          },
-                                          style: ElevatedButton.styleFrom(
-                                              backgroundColor: Colors.brown
-                                          ),
-                                          child: const Text(
-                                              "Add to Cart",
-                                              style: TextStyle(
-                                                  fontSize: 16,
-                                                  color: Colors.white
-                                              )
-                                          )
-                                      ),
-                                    )
-                                  ]
-                                ),
-                              )
-                            ]
-                          )
-                        ]
-                      ),
-                    )
-                  )
-                ],
+      appBar: AppBar(
+        title: const Text('kopilab.'),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 48),
+            child: IconButton(
+              icon: const badges.Badge(
+                badgeContent: Text(
+                  "0",
+                  style: TextStyle(color: Colors.brown),
+                ),
+                badgeAnimation: badges.BadgeAnimation.fade(toAnimate: false),
+                badgeStyle: badges.BadgeStyle(
+                  badgeColor: Colors.white,
+                ),
+                child: Icon(Icons.shopping_cart),
               ),
-            ]
+              onPressed: () {
+                context.pushNamed("cart");
+              },
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: FirebaseDatabaseQueryBuilder(
+          query: menuQuery,
+          builder: (context, snapshot, child) {
+            if (snapshot.isFetching) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (snapshot.hasError) {
+              return Text('Something went wrong! ${snapshot.error}');
+            }
+
+            Menu menu =
+                Menu.fromJson(snapshot.docs[0].value as Map<String, dynamic>);
+
+            var images = <Widget>[
+              Image.asset(menu.imageUrl),
+              Image.network(menu.imageUrl2),
+            ];
+
+            return ListView(
+              padding: const EdgeInsets.all(24),
+              children: [
+                images[0],
+                images[1],
+                Text(menu.name),
+                Text('Rp ${menu.price}'),
+                Text(menu.description),
+                Row(
+                  children: [
+                    Text('Item Quantity'),
+                    ElevatedButton(
+                      child: Text('-'),
+                      onPressed: () {},
+                    ),
+                    Text('1'),
+                    ElevatedButton(
+                      child: Text('+'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  child: Text('Add to cart - ${menu.price}'),
+                  onPressed: () {},
+                )
+              ],
+            );
+          },
         ),
       ),
     );
