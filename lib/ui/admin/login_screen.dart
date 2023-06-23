b648f7a968522fd596cdc03cc6bcbecafe8ae722
Ignatius Odi <ignodi@gmail.com>
2023-06-23 13:48:02 +0800

    update renderflex welcome and login screen

--- lib/ui/admin/login_screen.dart
+++ lib/ui/admin/login_screen.dart
@@ -21,142 +21,152 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                BackButton(onPressed: () => context.pop(), color: Colors.white),
-              ],
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 140),
-            child: const Text(
-              "kopilab.",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 60,
-                  fontWeight: FontWeight.w900),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(48),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20.0),
-                    topRight: Radius.circular(20.0)),
-                color: Color(0xFFFAFAFA),
-              ),
-              child: Column(
+      // resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  TextField(
-                    controller: _usernameController,
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      filled: true,
-                      fillColor: Colors.white,
-                      prefixIcon:
-                          const Icon(Icons.admin_panel_settings_outlined),
-                      hintText: "Username",
-                    ),
-                  ),
-                  const SizedBox(height: 24),
-                  TextField(
-                    obscureText: _obscureText,
-                    controller: _passwordController,
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      filled: true,
-                      fillColor: Colors.white,
-                      prefixIcon: const Icon(Icons.vpn_key_outlined),
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _obscureText = !_obscureText;
-                          });
-                        },
-                        icon: Icon(_obscureText
-                            ? Icons.visibility
-                            : Icons.visibility_off),
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      hintText: "Password",
-                    ),
-                  ),
-                  const SizedBox(height: 48),
-                  SizedBox(
-                    width: 400,
-                    height: 56,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.brown,
-                        foregroundColor: Colors.white,
-                        padding: const EdgeInsets.all(20),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                      onPressed: () async {
-                        bool valid = true;
-                        setState(() {
-                          _isLoading = true;
-                          _isError = !valid;
-                        });
-
-                        try {
-                          final username = _usernameController.text;
-                          final password = _passwordController.text;
-                          await _auth.signInWithEmailAndPassword(
-                            email: username,
-                            password: password,
-                          );
-
-                          if (context.mounted) {
-                            context.pushNamed("admin");
-                          }
-                        } on Exception catch (e) {
-                          print(e);
-                          valid = false;
-                        } finally {
-                          setState(() {
-                            _isLoading = false;
-                            _isError = !valid;
-                          });
-                        }
-                      },
-                      child: _isLoading
-                          ? const CircularProgressIndicator()
-                          : const Text(
-                              "LOGIN",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                    ),
-                  ),
-                  Text(
-                    _isError ? "No Credential Found!" : "",
-                    style: const TextStyle(
-                      color: Colors.redAccent,
-                      fontSize: 20,
-                    ),
-                  )
+                  BackButton(onPressed: () => context.pop(), color: Colors.white),
                 ],
               ),
             ),
-          ),
-        ],
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 88),
+              child: Text(
+                "kopilab.",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 60,
+                    fontWeight: FontWeight.w900),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: const Color(0xFFFAFAFA),
+              ),
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(48, 48, 48, 24),
+                    child: TextField(
+                      controller: _usernameController,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        prefixIcon:
+                            const Icon(Icons.admin_panel_settings_outlined),
+                        hintText: "Username",
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 48),
+                    child: TextField(
+                      obscureText: _obscureText,
+                      controller: _passwordController,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        prefixIcon: const Icon(Icons.vpn_key_outlined),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                          icon: Icon(_obscureText
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        hintText: "Password",
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(48, 60, 48, 24),
+                    child: SizedBox(
+                      width: 400,
+                      height: 56,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.brown,
+                          foregroundColor: Colors.white,
+                          padding: const EdgeInsets.all(20),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        onPressed: () async {
+                          bool valid = true;
+                          setState(() {
+                            _isLoading = true;
+                            _isError = !valid;
+                          });
+
+                          try {
+                            final username = _usernameController.text;
+                            final password = _passwordController.text;
+                            await _auth.signInWithEmailAndPassword(
+                              email: username,
+                              password: password,
+                            );
+
+                            if (context.mounted) {
+                              context.pushNamed("admin");
+                            }
+                          } on Exception catch (e) {
+                            print(e);
+                            valid = false;
+                          } finally {
+                            setState(() {
+                              _isLoading = false;
+                              _isError = !valid;
+                            });
+                          }
+                        },
+                        child: _isLoading
+                            ? const CircularProgressIndicator()
+                            : const Text(
+                                "LOGIN",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 48),
+                    child: Text(
+                      _isError ? "No Credential Found!" : "",
+                      style: const TextStyle(
+                        color: Colors.redAccent,
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/ui/welcome_screen.dart
+++ lib/ui/welcome_screen.dart
@@ -15,60 +15,61 @@ class WelcomeScreen extends StatelessWidget {
           fit: BoxFit.cover,
         )),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              IconButton(
-                icon: Image.asset("images/logo/logo.png"),
-                iconSize: 160,
-                onPressed: () {
-                  context.pushNamed("login");
-                },
-              ),
-              const SizedBox(height: 36),
-              const Text(
-                "kopilab.",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.w900,
-                  color: Colors.white,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  icon: Image.asset("images/logo/logo.png"),
+                  iconSize: 160,
+                  onPressed: () {
+                    context.pushNamed("login");
+                  },
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 60),
-                child: SizedBox(
-                  width: 284,
-                  child: Text(
-                    "As long as there was coffee in the world, how bad could things be?",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w100,
-                      color: Colors.white,
+                const SizedBox(height: 36),
+                const Text(
+                  "kopilab.",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.white,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 60),
+                  child: SizedBox(
+                    width: 284,
+                    child: Text(
+                      "As long as there was coffee in the world, how bad could things be?",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w100,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    fixedSize: const Size(240, 48),
                   ),
-                  fixedSize: const Size(240, 48),
-                ),
-                child: const Text(
-                  "Order Now",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                  child: const Text(
+                    "Order Now",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
+                  onPressed: () {
+                    context.pushNamed("home");
+                  },
                 ),
-                onPressed: () {
-                  context.pushNamed("home");
-                },
-              ),
-              const SizedBox(height: 160),
-            ],
+              ],
+            ),
           ),
         ),
       ),
